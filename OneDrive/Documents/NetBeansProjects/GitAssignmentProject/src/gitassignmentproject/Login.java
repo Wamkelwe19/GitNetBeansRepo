d402f975937b82860b24237d3aa02fc0127c1d4c
Wamkelwe19 <tshepoblessing24@gmail.com>
2026-05-01 14:10:38 +0200

    Add Login.java with login message on feature-login branch

---
+++ OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/gitassignmentproject/Login.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gitassignmentproject;
+
+/**
+ *
+ * @author tshep
+ */
+public class Login {
+     public static void main(String[] args) {
+        System.out.println("Login successful! Welcome to the system.");
+        System.out.println("User authenticated via Git branch: feature-login");
+    }
+    
+}
