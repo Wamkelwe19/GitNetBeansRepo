df7d0d0952a186033dc885c14a517bd298b951ca
Wamkelwe19 <tshepoblessing24@gmail.com>
2026-05-01 14:24:22 +0200

    Update message on main branch

--- OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/gitassignmentproject/Main.java
+++ OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/gitassignmentproject/Main.java
@@ -10,7 +10,7 @@ package gitassignmentproject;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+        System.out.println("Hello NetBeans. This is the MAIN branch version.");
     }
     
 }
