b1e10c3f8e01f79f6204531ba66bd81f22dc54d3
Wamkelwe19 <tshepoblessing24@gmail.com>
2026-05-01 09:15:15 +0200

    Intial commit - Hellow NetBeans message

---
+++ OneDrive/Documents/NetBeansProjects/GitAssignmentProject/src/gitassignmentproject/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitassignmentproject;
+
+/**
+ *
+ * @author tshep
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello NetBeans. Welcome to version control using Git");
+    }
+    
+}
